7c89c3aae85564448c868b61c1af112933b7078a
Fabian Vilers <fabian.vilers@dev-one.com>
2016-09-19 21:04:00 +0200

    Added favicons

--- app.js
+++ app.js
@@ -3,8 +3,11 @@
 const express = require("express");
 const path = require("path");
 const root = path.join(__dirname, "public");
+const favicon = require("serve-favicon");
 const app = express();
 
+// middlewares
+app.use(favicon(path.join(root, "favicon.ico"))); 
 app.use(express.static(root));
 app.all("*", (req, res) => {
   res.sendStatus(404);
