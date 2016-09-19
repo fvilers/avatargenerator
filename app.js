bce4bc0bc39ae3d35c2e787cd90eadf8c89048bd
Fabian Vilers <fabian.vilers@dev-one.com>
2016-09-19 19:25:46 +0200

    Added static files

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 "use strict";
 
 const express = require("express");
+const path = require("path");
+const root = path.join(__dirname, "public");
 const app = express();
 
+app.use(express.static(root));
 app.all("*", (req, res) => {
   res.sendStatus(404);
 });
