8a486e972068a9655fdf161bcaa33ee9ad2ff353
Fabian Vilers <fabian.vilers@dev-one.com>
2016-09-19 21:38:35 +0200

    Added compression

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 "use strict";
 
 const express = require("express");
+const compression = require("compression");
 const path = require("path");
 const root = path.join(__dirname, "public");
 const favicon = require("serve-favicon");
 const app = express();
 
 // middlewares
+app.use(compression());
 app.use(favicon(path.join(root, "favicon.ico"))); 
 app.use(express.static(root));
 app.all("*", (req, res) => {
